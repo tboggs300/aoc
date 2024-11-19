e0eaaee968e14aa7126e17ebd411bd0c04dfcc7f
unknown <tboggs300@gmail.com>
2024-11-19 01:32:31 -0500

    2020 day 1 part 2

--- 2020/day_1/src/main.rs
+++ 2020/day_1/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let mut lines = Vec::new();
     let mut v1: u32 = 0;
     let mut v2: u32 = 0;
+    let mut v3: u32 = 0;
 
     for line in fs::read_to_string("../input.txt").unwrap().lines() {
         lines.push(line.to_string().parse::<u32>().unwrap());
@@ -12,12 +13,19 @@ fn main() {
     while let Some(x) = lines.pop() {
         let mut found = false;
         for y in &lines {
-            if x + y == 2020 {
-               found = true;
-               v1 = x;
-               v2 = *y;
-               break;
-           }
+            for z in &lines {
+                if y == z {
+                    continue;
+                }
+                if x + y + z == 2020 {
+                    found = true;
+                    v1 = x;
+                    v2 = *y;
+                    v3 = *z;
+                    break;
+                }
+
+            }
         }
 
        if found {
@@ -25,6 +33,7 @@ fn main() {
        }
     }
 
-    println!("{}", v1 * v2);
+    println!("{} {} {}", v1, v2, v3);
+    println!("{}", v1 * v2 * v3);
 
 }

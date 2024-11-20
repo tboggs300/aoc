ce7ce0cd0db802a1294fa826c9d41019086e2eca
unknown <tboggs300@gmail.com>
2024-11-20 07:50:22 -0500

    2020 day 2 part 2

--- 2020/day_2/src/main.rs
+++ 2020/day_2/src/main.rs
@@ -1,7 +1,6 @@
 use std::{fs, env};
 
 fn main() {
-    const RADIX: u32 = 10;
     let path = env::current_dir().unwrap().to_str().unwrap().to_string();
     let filepath = format!("{}/input.txt", path);
     let num = fs::read_to_string(filepath.clone()).unwrap().lines().filter(|line| {
@@ -12,12 +11,8 @@ fn main() {
         let max = nums.next().unwrap().parse::<usize>().unwrap();
         let c = split.next().unwrap().chars().next().unwrap();
         let check_string = split.next().unwrap();
-        let num_of_occ = check_string.chars().filter(|ch| &c == ch).count();
-        println!("{} {}: {}-{} = {}", check_string, c, min, max, num_of_occ);
-
-        num_of_occ >= min && num_of_occ <= max
-        // let c = arr[1];
-        // let check_string = arr[2];
+        let indices = check_string.match_indices(c).filter(|ci| ci.0 + 1 == min || ci.0 + 1 == max).count();
+        indices == 1
     }).count();
     println!("{}", filepath.to_string());
     println!("{}", num);
